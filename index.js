b7a7548984880c2dc2cceb019b23e1a2df632d70
Hannah Won <nowhananhwon@gmail.com>
2014-08-25 13:42:18 -0700

    Adds ability to add divs on button click

--- index.js
+++ index.js
@@ -15,19 +15,21 @@ var milk = new Grocery("milk", "dairy");
 var eggs = new Grocery("eggs", "meat");
 
 // $('button').on('click', function() {
-//   // var newItem = $('.newItemBox').val();
-//   // var newCategory = $('.newCategory').val();
-//   // // Grocery(newItem, newCategory);
-//   // alert(newItem);
-//   // alert(newCategory);
-//   // alert(listofAll);
-//   // alert(itemList);
-//   // alert(categoryList);
+//   var newItem = $('.newItemBox').val();
+//   var selectCategory = $('#categoryNameID').val();
+//   var newCategory = $('.newCategory').val();
+//   // Grocery(newItem, newCategory);
+//   alert(newItem);
+//   alert(selectCategory);
+//   alert(newCategory);
+//   alert(listofAll);
+//   alert(itemList);
+//   alert(categoryList);
 
 // });
 
-$(document).ready(function() {
-  $("#addButton").click(function() {
-    $(".newItem").append("<p>" + $("#itemNameID").val() + ": " + $("#categoryNameID").val() + "</p>")
+$("#addButton").click(function() {
+  alert("in function");
+  $("#groceryList").prepend("<p>STUFF</p>");
+  // $(".newItemBox").prepend("<p>" + $("#itemNameID").val() + ": " + $("#categoryNameID").val() + "</p>")
   })
-})
